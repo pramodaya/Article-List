310dc16e22fe097c7bf192376387605d28efb015
pramodaya <pramodayajayalath@gmail.com>
2022-12-30 19:56:40 +0200

    added in memory users

--- src/main/java/com/listarticle/article/config/security/SecurityConfig.java
+++ src/main/java/com/listarticle/article/config/security/SecurityConfig.java
@@ -4,11 +4,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager users(){
+
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        manager.createUser(User.withUsername("pramodaya")
+                .password("{noop}password")
+                .authorities("read", "write")
+                .roles("USER")
+                .build());
+
+        manager.createUser(User.withUsername("john")
+                .password("{noop}password")
+                .authorities("read", "write", "delete")
+                .roles("ADMIN")
+                .build());
+
+       return manager;
+    }
+
+
     @Bean
     public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
