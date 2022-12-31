e4763caf2f78470d419b4d7c828eccfed9e310e6
pramodaya <pramodayajayalath@gmail.com>
2022-12-31 11:32:57 +0200

    move static data to application.properties file

--- src/main/java/com/listarticle/article/Config/Security/SecurityConfig.java
+++ src/main/java/com/listarticle/article/Config/Security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.listarticle.article.Config.Security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,18 +13,41 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${spring.security.user.name}")
+    private String user;
+    @Value("${spring.security.user.password}")
+    private String userPassword;
+    @Value("${spring.security.user.role}")
+    private String userRole;
+    @Value("${spring.security.user.authorities}")
+    private String[] userAuthorities;
+
+    @Value("${spring.security.admin.name}")
+    private String admin;
+    @Value("${spring.security.admin.password}")
+    private String adminPassword;
+    @Value("${spring.security.admin.role}")
+    private String adminRole;
+    @Value("${spring.security.admin.authorities}")
+    private String[] adminAuthorities;
+
+    @Value("${application.route.articleRoute}")
+    private String articleRoute;
+    @Value("${application.route.statisticRoute}")
+    private String statisticRoute;
+
     @Bean
     public InMemoryUserDetailsManager users(){
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("pramodaya")
-                .password("{noop}password")
-                .authorities("read", "write")
-                .roles("USER")
+        manager.createUser(User.withUsername(user)
+                .password(userPassword)
+                .authorities(userAuthorities)
+                .roles(userRole)
                 .build());
-        manager.createUser(User.withUsername("john")
-                .password("{noop}password")
-                .authorities("read", "write", "delete")
-                .roles("ADMIN")
+        manager.createUser(User.withUsername(admin)
+                .password(adminPassword)
+                .authorities(adminAuthorities)
+                .roles(adminRole)
                 .build());
 
        return manager;
@@ -34,8 +58,8 @@ public class SecurityConfig {
         httpSecurity
                 .csrf().disable()
                 .authorizeRequests()
-                .requestMatchers("/api/v1/article").hasRole("USER")
-                .requestMatchers("/api/v1/article/statistic").hasRole("ADMIN")
+                .requestMatchers(articleRoute).hasRole(userRole)
+                .requestMatchers(statisticRoute).hasRole(adminRole)
                 .anyRequest()
                 .authenticated();
         httpSecurity.formLogin();
